e85d7d8d372ca1d5cd54fce117f76873547be128
houstongolden <houstongolden@gmail.com>
2016-01-04 22:07:21 -0800

    changed the app id in const which was set to other app strangely

--- app/config/consts.js
+++ app/config/consts.js
@@ -3,7 +3,7 @@
     .constant('CONSTS', {
       anonymousToken: 'd6235176-7cb8-477c-abf5-b4b9e1d12af1',
       signUpToken: '60aaf611-a262-4cd6-abf2-e61ca62999c2',
-      appName: 'mytodo1'
+      appName: 'todopro'
     });
 
 })();
